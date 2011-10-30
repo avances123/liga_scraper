f78f41a616fab085fe6ae7ebb1c3c871c830ca6a
Fabio Rueda <avances123@gmail.com>
2011-10-31 00:50:20 +0100

    en json

--- liga_scraper/spiders/laliga_scraper_spider.py
+++ liga_scraper/spiders/laliga_scraper_spider.py
@@ -6,7 +6,10 @@ from liga_scraper.items import LigaScraperItem
 class DmozSpider(BaseSpider):
 	name = "ligayahoo"
 	allowed_domains = ["http://es.laliga.fantasysports.yahoo.com"]
-	start_urls = ["http://es.laliga.fantasysports.yahoo.com/football/62094/topplayers?sort_by=points&position=F&week=&season=2011"]
+	start_urls = ["http://es.laliga.fantasysports.yahoo.com/football/62094/topplayers?sort_by=points&position=F&week=&season=2011",
+		      "http://es.laliga.fantasysports.yahoo.com/football/62094/topplayers?sort_by=points&position=D&week=&season=2011",
+		      "http://es.laliga.fantasysports.yahoo.com/football/62094/topplayers?sort_by=points&position=M&week=&season=2011",
+		      "http://es.laliga.fantasysports.yahoo.com/football/62094/topplayers?sort_by=points&position=G&week=&season=2011"]
 
 	def parse(self, response):
 		items = []
@@ -14,11 +17,11 @@ class DmozSpider(BaseSpider):
 		players = hxs.select('//div[1]/div/div[2]/table/tbody/tr[td]')
 		for player in players:
 			item = LigaScraperItem()
-			item['posicion'] = player.select('//div[1]/div/div[2]/table/tbody/tr[td]/*[4]/text()').extract()
-			item['jugador'] = hxs.select('//div[1]/div/div[2]/table/tbody/tr[td]/*[2]/a/text()').extract()
-			item['equipo'] = hxs.select('//div[1]/div/div[2]/table/tbody/tr[td]/*[3]/text()').extract()
-			item['precio_mercado'] = hxs.select('//div[1]/div/div[2]/table/tbody/tr[td]/*[5]/text()').extract()
-			item['puntos'] = hxs.select('//div[1]/div/div[2]/table/tbody/tr[td]/*[6]/text()').extract()
+			item['posicion'] = player.select('td[4]/text()').extract()
+			item['jugador'] = player.select('td[2]/a/text()').extract()
+			item['equipo'] = player.select('td[3]/text()').extract()
+			item['precio_mercado'] = player.select('td[5]/text()').extract()
+			item['puntos'] = player.select('td[6]/text()').extract()
 			items.append(item)
 		
 		return items
